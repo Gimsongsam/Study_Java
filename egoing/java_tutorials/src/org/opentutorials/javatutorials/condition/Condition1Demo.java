af0cb5c46fa4bf72e77587985df0573e40407593
Gimsongsam <gimsong83@gmail.com>
2022-01-21 09:56:59 +0900

    egoin님 강의 study update

---
+++ egoing/java_tutorials/src/org/opentutorials/javatutorials/condition/Condition1Demo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.condition;
+
+public class Condition1Demo {
+
+	public static void main(String[] args) {
+		// true면 출력 / false면 출력되지 않
+		if(false) {
+			System.out.println("result: true");
+		}
+
+	}
+
+}
