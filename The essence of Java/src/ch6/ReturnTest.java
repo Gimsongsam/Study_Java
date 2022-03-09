68dc3a4ea5e3933ebc765b56a929dec76661b7e4
seonghye.kim <seonghye.kim@cndfactory.com>
2022-03-09 17:42:18 +0900

    20220309 ReturnTest update

--- The essence of Java/src/ch6/ReturnTest.java
+++ The essence of Java/src/ch6/ReturnTest.java
@@ -5,18 +5,19 @@ public class ReturnTest {
         ReturnTest r = new ReturnTest();
 
         int result = r.add(3, 5);
-        System.out.println(result);
+        System.out.println(result);  // 8
 
         int[] result2 = {0};         // 배열을 생성하고 result2[0]의 값을 0으로 초기화
         r.add(3, 5, result2);  // 배열을 add메서드의 매개변수로 전달
-        System.out.println(result2[0]);
+        System.out.println(result2[0]); // 8
     }
 
     int add(int a, int b){
-        return a + b;
+        return a + b; // 반환값이 있다.
     }
 
     void add(int a, int b, int[] result){
         result[0] = a + b; // 매개변수로 넘겨받은 배열에 연산결과를 저장
+        // 반환 값이 없다.
     }
 }
