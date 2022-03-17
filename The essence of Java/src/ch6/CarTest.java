0a87206e1d37f0164eee168580b3311642d9830a
gimsongsam <gimsong83@gmail.com>
2022-03-17 23:58:35 +0900

    git reset

--- The essence of Java/src/ch6/CarTest.java
+++ The essence of Java/src/ch6/CarTest.java
@@ -5,13 +5,20 @@ class Car{
     String gearType;    // 변속기 종류 - auto(자동), manual(수동)
     int door;           // 문의 개수
 
-    Car() {}
-
-    Car(String c, String g, int d){
-        color = c;
-        gearType = g;
-        door = d;
+    Car(){
+        this("white", "auto", 4);
     }
+
+    Car(String color){
+        this(color, "auto", 4);
+    }
+
+    Car(String color, String gearType, int door){
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+    }
+
 }
 
 public class CarTest {
