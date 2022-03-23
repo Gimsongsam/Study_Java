36ab839a96fcef50b911f06e3cd31549e7d2f17d
Gimsongsam <gimsong83@gmail.com>
2022-03-23 21:32:40 +0900

    20220323 DocumentTest study update

---
+++ The essence of Java/src/ch6/DocumentTest.java
@@ -0,0 +1,22 @@
+package ch6;
+
+class Document{
+    static int count = 0;
+    String name;        // 문서명(Document name)
+
+    Document(){         // 문서 생성 시 문서명을 지정하지 않았을 때
+        this("제목없음" + ++count);
+    }
+
+    Document(String name){
+        this.name= name;
+    }
+}
+
+public class DocumentTest {
+    public static void main(String[] args) {
+        Document d1 = new Document();
+    }
+
+
+}
