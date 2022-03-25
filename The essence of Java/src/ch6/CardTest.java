8daf75d8ac89c36ba7f030eb6cbbcce5d0e8f4aa
gimsongsam <gimsong83@gmail.com>
2022-03-25 16:21:25 +0900

    20220325 DeckTest study update

--- The essence of Java/src/ch6/CardTest.java
+++ The essence of Java/src/ch6/CardTest.java
@@ -7,32 +7,57 @@ public class CardTest {
         System.out.println("Card.width = " + Card.height);
 
         Card c1 = new Card();
-        c1.kind = "Heart";
+        c1.str_kind = "Heart";
         c1.number = 7;
 
         // 인스턴스변수의 값을 변경한다.
         Card c2 = new Card();
-        c2.kind = "Spade";
+        c2.str_kind = "Spade";
         c2.number = 4;
 
-        System.out.println("c1은 " + c1.kind + ", " + c1.number +
+        System.out.println("c1은 " + c1.str_kind + ", " + c1.number +
                 "이며, 크기는 (" + c1.width + "," + c1.number + ")");
-        System.out.println("c2은 " + c2.kind + ", " + c2.number +
+        System.out.println("c2은 " + c2.str_kind + ", " + c2.number +
                 "이며, 크기는 (" + c2.width + "," + c2.number + ")");
         System.out.println("c1의 width와 heigth를 각각 50, 80으로 변경합니다.");
         c1.width = 50;
         c1.height = 80;
 
-        System.out.println("c1은 " + c1.kind + ", " + c1.number +
+        System.out.println("c1은 " + c1.str_kind + ", " + c1.number +
                 "이며, 크기는 (" + c1.width + "," + c1.number + ")");
-        System.out.println("c2은 " + c2.kind + ", " + c2.number +
+        System.out.println("c2은 " + c2.str_kind + ", " + c2.number +
                 "이며, 크기는 (" + c2.width + "," + c2.number + ")");
     }
 }
 
 class Card {
-    String kind;
-    int number;
     static int width = 100;
     static int height = 250;
-}
+    String str_kind;
+
+    static final int KIND_MAX = 4;  // 카드 무늬의 수
+    static final int NUM_MAX = 13;  // 무늬별 카드 수
+
+    static final int SPADE = 4;
+    static final int DIAMOND = 3;
+    static final int HEART = 2;
+    static final int CLOVER = 1;
+    int kind;
+    int number;
+
+    Card(){
+        this(SPADE, 1);
+    }
+
+    Card(int kind, int number){
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public String toString(){
+        String[] kinds = {"", "CLOVER", "HEART", "DIAMOND", "SPADE"};
+        String numbers = "0123456789XJQK";    // 숫자 10은 x로 표현
+
+        return "kind : " + kinds[this.kind] + ", number : " + numbers.charAt(this.number);
+    }   // toString()의 끝
+}   // Card 클래스의 끝
