0b069d6a64c117e605f3d3333c72b5ed7ac98361
seonghye.kim <seonghye.kim@cndfactory.com>
2022-04-14 10:05:20 +0900

    20220414 CastingTest1 study update

--- The_essence_of_Java/src/ch7/CastingTest1.java
+++ The_essence_of_Java/src/ch7/CastingTest1.java
@@ -5,12 +5,16 @@ public class CastingTest1 {
         Car car = null;
         FireEngine fe = new FireEngine();
         FireEngine fe2 = null;
+        FireEngine fe3 = null;
+        Ambulance am = new Ambulance();
 
         fe.water();
         // car.water();    // 컴파일 에러!! Car타입의 참조변수로는 water()를 호출할 수 없다.
         car = fe;   // car = (Car)fe; 에서 형변환이 생략된 형태다.
         fe2 = (FireEngine)car; // 자손타입 <-- 조상타입
         fe2.water();
+
+        // fe3 = (FireEngine)am; // 같은 자손끼리는 형변환이 불가능하다.
     }
 }
 
@@ -28,5 +32,11 @@ class Car{
 class FireEngine extends Car{   // 소방차
     void water(){   // 물을 뿌리는 기능
         System.out.println("water!!!");
+    }
+}
+
+class Ambulance extends Car{
+    void Siren(){
+        System.out.println("Siren~~~~");
     }
 }
