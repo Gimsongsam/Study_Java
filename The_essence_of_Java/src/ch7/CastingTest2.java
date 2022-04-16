013b6cf657bc5d114f3b3054095e760f18045252
seonghye.kim <seonghye.kim@cndfactory.com>
2022-04-16 16:32:08 +0900

    20220416 CastingTest2 study update

--- The_essence_of_Java/src/ch7/CastingTest2.java
+++ The_essence_of_Java/src/ch7/CastingTest2.java
@@ -3,15 +3,17 @@ package ch7;
 public class CastingTest2 {
     public static void main(String[] args) {
         Car car = new Car();
+//        Car car = new FireEngine(); // <- 와 같이 사용해야 실행 시 에러가 발생하지 않는다.
         Car car2 = null;
         FireEngine fe = null;
 
         car.drive();
-        fe = (FireEngine)car;
+        fe = (FireEngine)car; // 조상타입의 인스턴스를 자손타입의 참조변수로 참조하는 것은 허용되지 않는다.
+                              // 컴파일은 OK, 실행 시 에러가 발생한다.
+                              // 변수 car가 조상의 인스턴스클래스 Car를 참조하고있다.
         fe.drive();
-
         fe.water();
         car2 = fe;
-        // car2.diver(); // 조상타입의 인스턴스를 자손타입의 참조변수로 참조하는 것은 허용되지 않는다.
+        car2.drive();
     }
 }
