42b7ac4f838a1e23b941fbbabad16d506767508c
seonghye.kim <seonghye.kim@cndfactory.com>
2022-04-21 09:07:23 +0900

    20220421 study update

--- The_essence_of_Java/src/ch7/polyArgumentTest.java
+++ The_essence_of_Java/src/ch7/polyArgumentTest.java
@@ -26,11 +26,17 @@ class AppleWatch extends Product{
     public String toString(){return "AppleWatch";}
 }
 
+class iPad extends Product{
+    iPad(){super(200);}
+
+    public String toString(){return "iPad";}
+}
+
 class Buyer{        // 고객, 물건을 사는 사람
     int money = 1000;   // 소유금액
     int bounsPoint = 0; // 보너스점수
-//    Product[] item = new Product[10];   // 구입한 제품을 저장하기 위한 배열
-//    int i = 0;                          // Product배열 item에 사용될 index
+    Product[] item = new Product[10];   // 구입한 제품을 저장하기 위한 배열
+    int i = 0;                          // Product배열 item에 사용될 index
 
     void buy(Product p){
         if(money < p.price){
@@ -40,8 +46,22 @@ class Buyer{        // 고객, 물건을 사는 사람
 
         money -= p.price;               // 가진 돈에서 구입한 제품의 가격을 뺀다.
         bounsPoint += p.bonusPoint;     // 제품의 보너스 점수를 추가한다.
-//        item[i++] = p;                  // 제품을 Product[] item에 저장한다.
+        item[i++] = p;                  // 제품을 Product[] item에 저장한다.
         System.out.println(p + "을/를 구입하셨습니다.");
+    }
+
+    void summary(){             // 구매한 물품에 대한 정보를 요약해서 보여준다.
+        int sum = 0;            // 구입한 물품의 가격합계
+        String itemList = "";   // 구입한 물품목록
+
+        // 반복문을 이용해서 구입한 물품의 총 가격과 목록을 만든다.
+        for(int i=0; i<item.length; i++){
+            if(item[i]==null) break;
+            sum += item[i].price;
+            itemList += item[i] + ", ";
+        }
+        System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
+        System.out.println("구입하신 제품은 " + itemList + "입니다.");
     }
 }
 
@@ -51,6 +71,7 @@ public class polyArgumentTest {
 
         b.buy(new Tv());
         b.buy(new AppleWatch());
+        b.buy(new iPad());
 
         System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
         System.out.println("현재 보너스점수는 " + b.bounsPoint + "점입니다.");
