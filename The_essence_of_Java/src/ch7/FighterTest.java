19c1dce647cd12d2f0b3189fcd6683627f52e219
seonghye.kim <seonghye.kim@cndfactory.com>
2022-04-27 09:48:40 +0900

    20220427 study update

--- The_essence_of_Java/src/ch7/FighterTest.java
+++ The_essence_of_Java/src/ch7/FighterTest.java
@@ -2,15 +2,25 @@ package ch7;
 
 public class FighterTest {
     public static void main(String[] args) {
+        Fighter f = new Fighter();
+        StartCraft s = new StartCraft();
 
+        f.move(1, 5);
+//        f.attack(StartCraft.Marine);  // ??
     }
+
+
 }
 
 class Fighter extends Unit implements Fightable{
-    public void move(int x, int y){/* 내용 생략 */}
-    public void attack(Unit u) { /* 내용 생략 */ }
+    public void move(int x, int y){
+        System.out.println(x + y + "으로 이동했습니다.");
+    }
+    public void attack(StartCraft u) {
+        System.out.println(u + "(이)가 공격을 시작했습니다.");
+    }
 }
 
 interface Fightable extends Movable, Attackable{}
 interface Movable { void move(int x, int y); }
-interface Attackable { void attack(Unit u); }
+interface Attackable { void attack(StartCraft u); }
--- The_essence_of_Java/src/ch7/StartCraft.java
+++ The_essence_of_Java/src/ch7/StartCraft.java
@@ -4,6 +4,7 @@ abstract class Unit{
     int x, y;
     abstract void move (int x, int y);
     void stop() { /* 현재 위치에 정지 */}
+    String name;
 
     int currentHP;  // 유닛의 체력
 }
@@ -13,6 +14,8 @@ public class StartCraft {
 
      static class Marine extends Unit {   // 보병
         int x, y;       // 현재 위치
+        String name = "Marin";
+
          // 이동하는 방법이 유닛별로 서로 달라서 move 메서드의 실제 구현 내용이 다를 것이다.
         void move(int x, int y) {
             System.out.println("move! move!");
@@ -27,6 +30,7 @@ public class StartCraft {
 
     static class Tank extends Unit {  // 탱크
         int x, y;       // 현재 위치
+        String name = "Tank";
 
         // 이동하는 방법이 유닛별로 서로 달라서 move 메서드의 실제 구현 내용이 다를 것이다.
         void move(int x, int y) {
@@ -42,6 +46,8 @@ public class StartCraft {
 
     static class DropShip extends Unit{
         int x, y;       // 현재 위치
+        String name = "DropShip";
+
         // 이동하는 방법이 유닛별로 서로 달라서 move 메서드의 실제 구현 내용이 다를 것이다.
         void move(int x, int y) {
             System.out.println("move! move!");
@@ -63,8 +69,6 @@ public class StartCraft {
         static class StaticTest{
 
         }
-
-
 //        class InstanceTest{   // ??
 //
 //        }
