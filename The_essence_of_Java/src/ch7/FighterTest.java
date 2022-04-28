a0c1e599646cac56d460fb4c4f89635e5b6dc804
seonghye.kim <seonghye.kim@cndfactory.com>
2022-04-28 16:33:23 +0900

    20220428 study update

--- The_essence_of_Java/src/ch7/FighterTest.java
+++ The_essence_of_Java/src/ch7/FighterTest.java
@@ -7,13 +7,25 @@ public class FighterTest {
 
         f.move(1, 5);
 //        f.attack(StartCraft.Marine);  // ??
+
+        if(f instanceof Unit)
+            System.out.println("f는 Unit클래스의 자손입니다.");
+        if(f instanceof Fightable)
+            System.out.println("f는 Fightable인터페이스를 구현했습니다.");
+        if(f instanceof Movable)
+            System.out.println("f는 Movable인터페이스를 구현했습니다.");
+        if(f instanceof Attackable)
+            System.out.println("f는 Attackable인터페이스를 구현했습니다.");
+        if(f instanceof Object)
+            System.out.println("f는 Object 클래스의 자손입니다.");
     }
 
 
 }
 
-class Fighter extends Unit implements Fightable{
-    public void move(int x, int y){
+class Fighter extends Unit implements Fightable{    // Unit 클래스로부터 상속, Fightable 인터페이스만을 구현
+    public void move(int x, int y){     // 제어자를 public으로 했다.
+                                        // 오버라이딩을 할 때는 조상의 메서드보다 넓은 범위의 접근 제어자를 지정해야 한다.
         System.out.println(x + y + "으로 이동했습니다.");
     }
     public void attack(StartCraft u) {
@@ -22,5 +34,5 @@ class Fighter extends Unit implements Fightable{
 }
 
 interface Fightable extends Movable, Attackable{}
-interface Movable { void move(int x, int y); }
+interface Movable { void move(int x, int y); } // public abstract가 생략되어있다.
 interface Attackable { void attack(StartCraft u); }
