b9c7c517d32304851fb3eeb5f03d85fed9e5f9c5
seonghye.kim <seonghye.kim@cndfactory.com>
2022-05-19 09:16:01 +0900

    20220519 study update

--- The_essence_of_Java/src/ch7/DefaultMethodTest.java
+++ The_essence_of_Java/src/ch7/DefaultMethodTest.java
@@ -22,8 +22,19 @@ interface MyInterface{
     default void method1(){
         System.out.println("method1() in MyInterface");
     }
+    default void method2(){
+        System.out.println("method2() in MyInterface");
+    }
+    static void staticMethod(){
+        System.out.println("staticMethod() in MyInterface");
+    }
 }
 
 interface MyInterface2{
-
+    default void method1(){
+        System.out.println("method1() in MyInterface2");
+    }
+    static void staticMethod(){
+        System.out.println("staticMethod() in MyInterface2");
+    }
 }
