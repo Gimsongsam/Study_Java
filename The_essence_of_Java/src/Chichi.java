504ff705b22b89dd096790dbde70cc96f7b1b599
seonghye.kim <seonghye.kim@cndfactory.com>
2022-05-21 16:41:55 +0900

    20220521 study update

---
+++ The_essence_of_Java/src/Chichi.java
@@ -0,0 +1,37 @@
+public class Chichi {
+    // main method
+    public static void main(String[] args){
+        boolean flag = true;
+        String test = "Y";  // variable
+        String infoTest = "N";
+
+        boolean sh = flag ? true : false;
+
+        if(flag ? true : false) {
+            System.out.println("i love u");
+        } else {
+            System.out.println("i hate u");
+        }
+
+        System.out.println("i love u");
+
+        if( "Y".equals(test) ? false : "N".equals(infoTest)){   // ??? variable x , method x
+            System.out.println("chichi");
+        }else{
+            System.out.println("song");
+        }
+    }
+
+    //"Y".equals(item.getString("weighYn")) ? false : "N".equals(item.getString("orderInfo15")
+
+
+
+
+
+    // member variable
+    // member method
+
+
+
+
+}
--- The_essence_of_Java/src/ch7/DefaultMethodTest.java
+++ The_essence_of_Java/src/ch7/DefaultMethodTest.java
@@ -2,19 +2,25 @@ package ch7;
 
 public class DefaultMethodTest {
     public static void main(String[] args) {
+        Child2 c2 = new Child2();
 
+        c2.method1();
+        c2.method2();
+//        c2.staticMethod();
+        MyInterface.staticMethod();
+        MyInterface2.staticMethod();
     }
 }
 
-class Child2 extends Parent implements MyInterface, MyInterface2{
+class Child2 extends Parent2 implements MyInterface, MyInterface2{
     public void method1(){
-        System.out.println("method1() in Child");   // 오버라이딩
+        System.out.println("method1() in Child2");   // 오버라이딩
     }
 }
 
 class Parent2{
     public void method2(){
-        System.out.println("method2() in Parent");
+        System.out.println("method2() in Parent2");
     }
 }
 
