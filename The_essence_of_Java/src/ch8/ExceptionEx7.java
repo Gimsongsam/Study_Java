022a7135b25b3d1d5df072fd1bea92bc3d87b574
seonghye.kim <seonghye.kim@cndfactory.com>
2022-06-04 12:55:57 +0900

    20220604 ExceptionEx7 study update

---
+++ The_essence_of_Java/src/ch8/ExceptionEx7.java
@@ -0,0 +1,22 @@
+package ch8;
+
+public class ExceptionEx7 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        try{
+            System.out.println(3);
+            System.out.println(0/0);
+            System.out.println(4);  // 실행되지 않는다.
+        } catch(ArithmeticException ae){
+            if(ae instanceof ArithmeticException)
+                System.out.println("true");
+            if(ae instanceof Exception)
+                System.out.println("ae is Exception instance");
+            System.out.println("ArithmeticException");
+        } catch (Exception e){
+            System.out.println("Exception");
+        }   // try-catch의 끝
+        System.out.println(6);
+    }   // main메서드의 끝
+}
