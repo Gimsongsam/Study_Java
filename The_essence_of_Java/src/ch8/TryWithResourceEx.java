f95d2348bee02874c38d09dbdbf330d828d5f6e5
Gimsongsam <gimsong83@gmail.com>
2022-07-27 23:30:47 +0900

    20220727 TryWithResourceEx study update

--- The_essence_of_Java/src/ch8/TryWithResourceEx.java
+++ The_essence_of_Java/src/ch8/TryWithResourceEx.java
@@ -4,10 +4,23 @@ public class TryWithResourceEx {
     public static void main(String[] args) {
         try(CloseableResource cr = new CloseableResource()){
             cr.exceptionWork(false); // 예외가 발생하지 않는다.
+        } catch(WorkException e){
+            e.printStackTrace();
+        } catch(CloseException e){
+            e.printStackTrace();
         }
-    }
+        System.out.println();
 
-    class CloseableResource implements AutoCloseable{
+        try(CloseableResource cr = new CloseableResource()){
+            cr.exceptionWork(true); // 예외가 발생한다.
+        } catch(WorkException e){
+            e.printStackTrace();
+        }catch (CloseException e){
+            e.printStackTrace();
+        }
+    }   // main의 끝
+
+    static class CloseableResource implements AutoCloseable{
         public void exceptionWork(boolean exception) throws WorkException{
             System.out.println("exceptionWork("+exception+")가 호출됨");
 
@@ -21,11 +34,11 @@ public class TryWithResourceEx {
         }
     }
 
-    class WorkException extends Exception{
+    static class WorkException extends Exception{
         WorkException(String msg){super(msg);}
     }
 
-    class CloseException extends Exception{
+    static class CloseException extends Exception{
         CloseException(String msg){super(msg);};
     }
 }
