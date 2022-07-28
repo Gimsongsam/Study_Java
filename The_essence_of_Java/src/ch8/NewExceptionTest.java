9b74f76bb63a4935f7a45aaaa399b89bdfd18d81
Gimsongsam <gimsong83@gmail.com>
2022-07-28 23:11:27 +0900

    20220728 NewExceptionTest study update

--- The_essence_of_Java/src/ch8/NewExceptionTest.java
+++ The_essence_of_Java/src/ch8/NewExceptionTest.java
@@ -3,11 +3,22 @@ package ch8;
 public class NewExceptionTest {
     public static void main(String[] args) {
         try{
-
+            startInstall();     // 프로그램 설치에 필요한 준비를 한다.
+            copyFiles();        // 파일들을 복사한다.
+        } catch (SpaceException e){
+            System.out.println("에러 메시지: " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("공간을 확보한 후에 다시 설치하시기 바랍니다.");
+        } catch (MemoryException me){
+            System.out.println("에러 메시지 : " + me.getMessage());
+            me.printStackTrace();
+            System.gc();    // Garbage Coolection을 수행하여 메모리를 늘려준다.
+            System.out.println("다시 설치를 시도하세요.");
         } finally {
-
-        }
+            deleteTempFiles();  // 프로그램 설치에 사용된 임시파일들을 삭제한다.
+        } // try의 끝
     }   // main
+
     static void startInstall() throws SpaceException, MemoryException{
         if(!enoughSpace())  // 충분한 설치 공간이 없으면
             throw new SpaceException("설치할 공간이 부족합니다.");
