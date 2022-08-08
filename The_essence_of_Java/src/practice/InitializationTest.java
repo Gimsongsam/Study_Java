9391fde9cd87008ba25c9d77dc1de919a043bd65
Gimsongsam <gimsong83@gmail.com>
2022-08-08 23:35:18 +0900

    20220808 InitializationTest study update

--- The_essence_of_Java/src/practice/InitializationTest.java
+++ The_essence_of_Java/src/practice/InitializationTest.java
@@ -3,33 +3,36 @@ package practice;
 class Initialization {
     int a;
     static int b;
+    static int c;
 
     // 초기화 블럭
     {
         a++;
         b++;
     }
+
+    static{
+        c++;
+    }
 }
 
 public class InitializationTest{
     public static void main(String[] args) {
 //        Initialization soutA1 = new Initialization();
-//        System.out.println(soutA1.b);
-//
 //        Initialization soutA2 = new Initialization();
-//        System.out.println(soutA2.b);
-//
 //        Initialization soutA3 = new Initialization();
-//        System.out.println(soutA3.b);
+//        System.out.println(Initialization.b);
+//        System.out.println(Initialization.b);
+//        System.out.println(Initialization.b);
 
         // 스테틱
         System.out.println("static1");
         Initialization soutA1 = new Initialization();
-        System.out.println(soutA1.b);
+        System.out.println(Initialization.b);
         Initialization soutA2 = new Initialization();
-        System.out.println(soutA2.b);
+        System.out.println(Initialization.b);
         Initialization soutA3 = new Initialization();
-        System.out.println(soutA3.b);
+        System.out.println(Initialization.b);
 
         // 인스턴스는 생성될때 마다 초기화
         System.out.println("Instant");
