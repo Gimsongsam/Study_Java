52b97a1ab38f69319cca50e3ba4fb5e7843674f1
seonghye.kim <seonghye.kim@cndfactory.com>
2022-08-10 09:31:35 +0900

    20220809 Polymorphism study update

--- The_essence_of_Java/src/practice/Polymorphism.java
+++ The_essence_of_Java/src/practice/Polymorphism.java
@@ -18,7 +18,7 @@ class Tv {
 }
 class CaptionTv extends Tv{
     String text;
-    void caption(){/* 내용생략 */};
+    void caption(){ System.out.println("caption()"); }
 }
 
 public class Polymorphism {
@@ -35,6 +35,12 @@ public class Polymorphism {
         // 자손이 조상타입의 인스턴스를 참조하는 것은 안된다.
         // CaptionTv ct = new Tv();
 
+        // 형변환을 해야 사용가능하다.
+        CaptionTv ct = (CaptionTv)t;
+        ct.caption();
+        // 형변환은 참조변수의 타입을 변환하는 것이다.
+        // 참조하고 있는 인스턴스에서 사용할 수 있는 멤버의 범위(개수)를 조절하는 것이다.
 
+        System.out.println(ct instanceof CaptionTv);
     }
 }
