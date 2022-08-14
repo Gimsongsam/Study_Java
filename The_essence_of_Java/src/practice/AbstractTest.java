790e82f6734639006a182dea7bdea44f421c1dc0
Gimsongsam <gimsong83@gmail.com>
2022-08-14 11:04:17 +0900

    20220814 AbstractTest study update

--- The_essence_of_Java/src/practice/AbstractTest.java
+++ The_essence_of_Java/src/practice/AbstractTest.java
@@ -6,5 +6,11 @@ abstract class Test { // 추상클래스
 
 
 public class AbstractTest {
+    public static void main(String[] args) {
+        check("test");
+    }
 
+    static void check(String a){
+        System.out.println("true");
+    }
 }
