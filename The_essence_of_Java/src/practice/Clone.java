d9872a97b1b5042c8cbc32965235cd04ab08decc
seonghye.kim <seonghye.kim@cndfactory.com>
2022-08-17 08:47:03 +0900

    20220817 Clone study update

--- The_essence_of_Java/src/practice/Clone.java
+++ The_essence_of_Java/src/practice/Clone.java
@@ -6,6 +6,11 @@ class Circle implements Cloneable{
     Point p; // 원점 - 참조변수
     double r; // 반지름
 
+    Circle(Point p, double r){
+        this.p = p;
+        this.r = r;
+    }
+
     public Circle clone(){ // 얕은 복사
         Object obj = null;
         try{
@@ -16,5 +21,10 @@ class Circle implements Cloneable{
 }
 
 public class Clone {
-
+    public static void main(String[] args) {
+        // c1과 c2는 같은 Point 인스턴스를 가리키게 된다.
+        // 따라서 완전한 복제라고 볼 수 없다.
+        Circle c1 = new Circle(new Point(1, 1), 2.0);
+        Circle c2 = c1.clone(); // 얕은 복사
+    }
 }
