a55dd9d31413b4436388f23edb6a16fd906a11ef
seonghye.kim <seonghye.kim@cndfactory.com>
2022-08-18 20:42:40 +0900

    20220818 ShallowDeepCopy study update

--- The_essence_of_Java/src/ch9/ShallowDeepCopy.java
+++ The_essence_of_Java/src/ch9/ShallowDeepCopy.java
@@ -1,5 +1,38 @@
 package ch9;
 
-public class ShallowDeepCopy {
+class Circle implements Cloneable{
+    Point p; // 원점
+    double r; // 반지름
 
+    Circle(Point p, double r){
+        this.p = p;
+        this.r = r;
+    }
+
+    public Circle shallowCopy(){ // 얕은 복사
+        Object obj = null;
+
+        try{
+            obj = super.clone();
+        } catch (CloneNotSupportedException e){}
+
+        return (Circle) obj;
+    }
+
+    public Circle deepCopy(){ // 깊은 복사
+        Object obj = null;
+        try{
+            obj = super.clone();
+        } catch (CloneNotSupportedException e){}
+
+
+
+        return (Circle) obj;
+    }
+}
+
+public class ShallowDeepCopy {
+    public static void main(String[] args) {
+
+    }
 }
