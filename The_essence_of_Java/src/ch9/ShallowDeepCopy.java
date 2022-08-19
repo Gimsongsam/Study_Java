523fada88374ba73dda824c902643412d955b0a4
shkim <gimsong83@gmail.com>
2022-08-19 16:37:16 +0900

    20220819 ShallowDeepCopy study update

--- The_essence_of_Java/src/ch9/ShallowDeepCopy.java
+++ The_essence_of_Java/src/ch9/ShallowDeepCopy.java
@@ -25,14 +25,35 @@ class Circle implements Cloneable{
             obj = super.clone();
         } catch (CloneNotSupportedException e){}
 
+        Circle c = (Circle)obj;
+        c.p = new Point(this.p.x, this.p.y);
 
+        return c;
+    }
 
-        return (Circle) obj;
+    public String toString(){
+        return "[p=" + p + ", r=" + r +"]";
+    }
+
+    class Point{
+        int x, y;
+
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public String toString(){
+            return "{"+ x +", " + y + ")";
+        }
     }
 }
 
 public class ShallowDeepCopy {
     public static void main(String[] args) {
+        Circle c1 = new Circle(new Point(1, 1), 2.0);
+        Circle c2 = c1.clone();
+
 
     }
 }
