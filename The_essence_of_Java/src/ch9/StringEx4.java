8cefe53caaa63ea402ff6e3978bb4ba4fac6cf7a
Gimsongsam <gimsong83@gmail.com>
2022-09-06 08:56:21 +0900

    20220906 String study update

--- The_essence_of_Java/src/ch9/StringEx4.java
+++ The_essence_of_Java/src/ch9/StringEx4.java
@@ -2,11 +2,20 @@ package ch9;
 
 // join()은 여러 문자열 사이에 구분자를 넣어서 결합한다.
 
+import java.util.StringJoiner;
+
 public class StringEx4 {
     public static void main(String[] args) {
         String animals = "dog,cat,bear";
         String[] arr = animals.split(",");
 
-//        System.out.println(String.join);
+        System.out.println(String.join("-", arr));
+
+        StringJoiner sj = new StringJoiner("/","[","]");
+        for(String s: arr)
+            sj.add(s);
+
+        System.out.println(sj.toString());
+
     }
 }
