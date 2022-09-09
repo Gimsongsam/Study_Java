7bbb05e4e7c5102d8a2cfb76326060efb1019557
shkim <gimsong83@gmail.com>
2022-09-09 10:07:54 +0900

    20220908 QuickSort study update

--- The_essence_of_Java/src/practice/QuickSort.java
+++ The_essence_of_Java/src/practice/QuickSort.java
@@ -1,2 +1,30 @@
-package practice;public class QuickSort {
+package practice;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5,2,7,4,0,9,3};
+
+//        System.out.println(arr.);
+
+//        LowSearch(arr);
+    }
+
+    // low를 탐색한다
+    public static void LowSearch(int[] arr){
+
+        // 배열의 첫번째 인덱스를 pivot으로 뽑는다.
+        int l_pivot = arr[0];
+
+        // 탐색시작
+        for(int i=0; i<arr.length; i++){
+            //비교할 숫자가 없으면 탐색 중지
+            if(i==arr.length){
+
+            }
+
+            // pivot보다 큰 숫자를 만나면 탐색중지
+
+        }
+
+    }
 }
