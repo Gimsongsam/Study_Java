729fc7863a8b650b3e5e1512d70b29ed7797eda8
shkim <gimsong83@gmail.com>
2022-09-12 18:06:12 +0900

    20220912 MathEx1 study edit

--- The_essence_of_Java/src/ch9/MathEx1.java
+++ The_essence_of_Java/src/ch9/MathEx1.java
@@ -1,5 +1,6 @@
 package ch9;
 
+// 코드를 간단히 하기 위해 정적 import문을 추가하였다.
 import static java.lang.Math.*;
 import static java.lang.System.*;
 
@@ -20,11 +21,17 @@ public class MathEx1 {
         out.printf("ceil(%3.1f)=%3.1f%n", 1.1, ceil(1.1)); // 올림
         out.printf("floor(%3.1f)=%3.1f%n", 1.5, floor(1.5)); // 버림
         out.printf("round(%3.1f)=%d%n", 1.1, round(1.1)); // 반올림
-        out.printf("ceil(%3.1f)=%3.1f%n", 1.5, ceil(1.5)); // 반올림
+
+        // rint()도 round()처럼 소수점 첫 째자리에서 반올림하지만, 반환값이 double이다.
+        out.printf("round(%3.1f)=%d%n", 1.5, round(1.5)); // 반올림
         out.printf("rint(%3.1f)=%3.1f%n", 1.5, rint(1.5)); // 반올림
+
+        // rint()는 두 정수의 정가운데 있는 값은 가장 가까운 짝수 정수를 반환한다.
         out.printf("round(%3.1f)=%d%n", -1.5, round(-1.5)); // 반올림
         out.printf("rint(%3.1f)=%3.1f%n", -1.5, rint(-1.5)); // 반올림
         out.printf("ceil(%3.1f)=%3.1f%n", -1.5, ceil(-1.5)); // 올림
+
+        // 양수와 달리 -1.5를 버림하면 -2.0이 된다.
         out.printf("floor(%3.1f)=%3.1f%n", -1.5, floor(-1.5)); // 버림
 
     }
