2547554127de3490e5ff70bf08a0d71cb6749e4c
shkim <gimsong83@gmail.com>
2022-09-14 20:30:22 +0900

    20220913 WrapperEx1 study edit

--- The_essence_of_Java/src/ch9/WrapperEx1.java
+++ The_essence_of_Java/src/ch9/WrapperEx1.java
@@ -6,10 +6,16 @@ public class WrapperEx1 {
         Integer i2 = new Integer(100);
 
         System.out.println("i==i2 ? "+(i==i2));
+
+        // 래퍼 클래스들은 모두 equals()가 오버라이딩되어 있다.
+        // 따라서 주소값이 아닌 객체가 가지고 있는 값을 비교한다.
         System.out.println("i.equals(i2) ? " + i.equals(i2));
+
+        // compareTo() 함수는 두개의 값을 비교하여 int 값으로 반환해주는 함수이다.
         System.out.println("i.compareTo(i2)="+i.compareTo(i2));
         System.out.println("i.toString()="+i.toString());
 
+        // toString도 오버라이딩되어 있어 객체가 가지고 있는 값을 문자열로 변환하여 반환한다.
         System.out.println("MAX_VALUE="+Integer.MAX_VALUE);
         System.out.println("MIN_VALUE="+Integer.MIN_VALUE);
         System.out.println("SIZE="+Integer.SIZE+" bits");
